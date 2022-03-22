896c56eac7467c38d893bdfcf5392409c53ca210
kayky_paulino <98046923+kaykypaulinoalvesdossantos@users.noreply.github.com>
2022-03-22 10:25:56 -0300

    Thema dark

---
+++ src/js/theme.js
@@ -0,0 +1,10 @@
+// change theme
+
+const themeBtn = document.querySelector('.theme-btn');
+
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+
+    themeBtn.querySelector('span:first-child').classList.toggle('active');
+    themeBtn.querySelector('span:last-child').classList.toggle('active');
+})
